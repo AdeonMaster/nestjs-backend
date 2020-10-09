2a6407ee2561602738afdc8c20217da9771763a2
AdeonMaster <adeonmaster@gmail.com>
2020-10-09 17:14:53 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app/app.module';
+
+const httpsOptions = {
+  key: fs.readFileSync(path.resolve('certificate/private.key')),
+  cert: fs.readFileSync(path.resolve('certificate/cert.crt')),
+};
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
+  const options = new DocumentBuilder()
+    .setTitle('Backend Template')
+    .setDescription('Needs a better description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(3000);
+}
+
+bootstrap();
